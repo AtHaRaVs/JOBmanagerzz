efa8039214bba9da3a9e0bb3ec45d9d21d2627fe
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-10-15 14:57:47 +0530

    dashboard layout setting

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,10 +1,21 @@
 import { Outlet } from "react-router-dom";
+import { BigSidebar, SmallSidebar, Navbar } from "../components";
+import Wrapper from "../assets/wrappers/BigSidebar";
 
 const DashboardLayout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   );
 };
 export default DashboardLayout;
