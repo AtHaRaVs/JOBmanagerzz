2a63da991c8a3fb6628bd3ef3bb5a369ced81f14
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-10-16 19:08:00 +0530

    dashbaord context intilization

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,8 +1,26 @@
 import { Outlet } from "react-router-dom";
 import { BigSidebar, SmallSidebar, Navbar } from "../components";
-import Wrapper from "../assets/wrappers/BigSidebar";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { useState } from "react";
 
 const DashboardLayout = () => {
+  const user = { name: "Jhon" };
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    console.log("toggle dark theme");
+  };
+
+  const toggleSideBar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const logoutUser = async () => {
+    console.log("Logout User");
+  };
+
   return (
     <Wrapper>
       <main className="dashboard">
