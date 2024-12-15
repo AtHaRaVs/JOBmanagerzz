08edc91e67e489f03be1642edee51bbd2bad4976
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-12-15 11:20:21 +0530

    validating user register form

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,8 +3,9 @@ import { Router } from "express";
 const router = Router();
 
 import { register, login } from "../controllers/authControllers.js";
+import { validateRegisterInput } from "../middlewares/validationMiddleware.js";
 
-router.post("/register", register);
+router.post("/register", validateRegisterInput, register);
 router.post("/login", login);
 
 export default router;
