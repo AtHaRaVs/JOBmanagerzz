eccd347a845b8039fd2b302c70e7f841e85d1737
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-12-15 11:26:09 +0530

    admin func made

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,8 @@ import User from "../models/userModel.js";
 import { StatusCodes } from "http-status-codes";
 
 export const register = async (req, res) => {
+  const isFirstUser = (await User.countDocuments()) === 0;
+  req.body.role = isFirstUser ? "admin" : "user";
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
