6135002939b9277e3497188bd896037a3eca53f0
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-12-15 13:02:17 +0530

    password hashing sent to utils

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,13 +1,12 @@
 import User from "../models/userModel.js";
 import { StatusCodes } from "http-status-codes";
-import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
   const isFirstUser = (await User.countDocuments()) === 0;
   req.body.role = isFirstUser ? "admin" : "user";
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
