ce279a5f08e1ad29ae7b3e4bbade87ac1221368f
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-12-17 11:55:27 +0530

    verifying jwt functionality made

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,9 +1,17 @@
 import { UnauthenticatedError } from "../errors/customError.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
   const { token } = req.cookies;
   if (!token) throw new UnauthenticatedError("authentication invalid");
 
-  console.log("authenticating user");
-  next();
+  try {
+    const { userId, role } = verifyJWT(token);
+
+    req.user = { userId, role };
+
+    next();
+  } catch (error) {
+    if (!token) throw new UnauthenticatedError("authentication invalid");
+  }
 };
